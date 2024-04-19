0694d758ea9701ca8f77576a33aba98d391e207f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-19 22:35:51 +0000

    Updating logos

---
+++ lib/pages/home_page_u_c/home_page_u_c_model.dart
@@ -0,0 +1,17 @@
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'home_page_u_c_widget.dart' show HomePageUCWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class HomePageUCModel extends FlutterFlowModel<HomePageUCWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
